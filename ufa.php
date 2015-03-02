f6ab4d89c62d268f2a095989a8f3ffe32ae426a2
trasek <trasek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-02 20:07:11 +0000

    Bugs repair, update
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-member-wp-user-frontend-addon/trunk@1103542 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ufa.php
+++ ufa.php
@@ -4,7 +4,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
  * Plugin Name: Ultimate Member + WP User Frontend - Addon
  * Plugin URI: http://devwp.pl/ultimate-member-wp-user-frontend-addon/
  * Description: Integration of "Ultimate Member" + "WP User Fronted" in user profiles
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: DevWP
  * Author URI: http://devwp.pl
  * Text Domain: ufa
@@ -13,7 +13,7 @@ if ( !class_exists( 'WeDevs_Settings_API' ) ) {
   require_once dirname( __FILE__ ) . '/lib/class.settings-api.php';
 }
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-if(is_plugin_active('ultimate-member')){
+if(is_plugin_active('ultimate-member/index.php')){
   require_once 'admin/options.php';
   require_once 'user-dashboard.php';
   require_once 'actions.php';
